a77b8bf376e280eef5d8d21e8e5882c6201b5877
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2023-01-28 19:58:33 -0600

    lazy commit message

--- MainFile.py
+++ MainFile.py
@@ -12,6 +12,31 @@ from dash_bootstrap_templates import load_figure_template
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 
+def sharpe_sortino_beta(ticker='AAPL', market_returns='SPY'):
+    try:
+        
+        ts = TimeSeries(key='SWKZ23Y8HKIF4N4A', output_format='pandas')
+        data, meta_data = ts.get_daily_adjusted(ticker)
+        returns = data['4. close'].pct_change()
+        mar = 0.03  # minimum acceptable return
+        downside_returns = returns[returns < mar]
+        mean_return = returns.mean()
+        std_return = returns.std()
+        downside_std = downside_returns.std()
+        sharpe_ratio = (mean_return - mar) / std_return
+        sortino_ratio = (mean_return - mar) / downside_std
+        data_market, meta_data = ts.get_daily_adjusted(market_returns)
+        market_returns = data_market['4. close'].pct_change()
+        beta_value = returns.cov(market_returns) / market_returns.var()
+        print("Sharpe Ratio:", sharpe_ratio)
+        print("Sortino Ratio:", sortino_ratio)
+        print("beta_value:",beta_value)
+        
+        return sharpe_ratio, sortino_ratio, beta_value
+    
+    except:
+        
+        return 0,0,0
 
 
 
@@ -62,31 +87,6 @@ def update_output_div(input_value):
     return f'Output: {input_value}'
 
 
-def sharpe_sortino_beta(ticker='AAPL', market_returns='SPY'):
-    try:
-        
-        ts = TimeSeries(key='SWKZ23Y8HKIF4N4A', output_format='pandas')
-        data, meta_data = ts.get_daily_adjusted(ticker)
-        returns = data['4. close'].pct_change()
-        mar = 0.03  # minimum acceptable return
-        downside_returns = returns[returns < mar]
-        mean_return = returns.mean()
-        std_return = returns.std()
-        downside_std = downside_returns.std()
-        sharpe_ratio = (mean_return - mar) / std_return
-        sortino_ratio = (mean_return - mar) / downside_std
-        data_market, meta_data = ts.get_daily_adjusted(market_returns)
-        market_returns = data_market['4. close'].pct_change()
-        beta_value = returns.cov(market_returns) / market_returns.var()
-        print("Sharpe Ratio:", sharpe_ratio)
-        print("Sortino Ratio:", sortino_ratio)
-        print("beta_value:",beta_value)
-        
-        return sharpe_ratio, sortino_ratio, beta_value
-    
-    except:
-        
-        return 0,0,0
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0',port=80)
