7e1eb29901f1d9de908d7bbd4fbc44b25fdb0b10
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2023-01-28 19:26:24 -0600

    lazy commit message

--- MainFile.py
+++ MainFile.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 
@@ -14,27 +15,27 @@ values = [50, 25, 25]
 
 fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
 
-
+load_figure_template('SLATE')
 
 app.layout = html.Div(children=[
     html.H1('GS + TAMUHACK2023 portfolio Risk optimizer\n', style={'textAlign': 'center'}),
 
     html.H2(children='''
         Welcome to risk mitigated finance. Simplified.\n 
-    ''', style={'textAlign': 'center'}),
+    ''', style={'textAlign': 'center', 'margin-top':'7px'}),
     
     html.H3(children='''
         There are many types of risk and exposure. Intuition would have it the safest and most conservative thing to do with the money you have saved up is nothing!\n
-    ''', style={'textAlign': 'center'}),
+    ''', style={'textAlign': 'center', 'margin-top':'7px'}),
     
     html.H3(children='''
         However, there are many factors that would potentially make this not the case.\n
-     ''', style={'textAlign': 'center'})#,
+     ''', style={'textAlign': 'center', 'margin-top':'7px'}),
 
-    # dcc.Graph(
-    #     id='portfolio composition',
-    #     figure=fig
-    # )
+    dcc.Graph(
+        id='portfolio composition',
+        figure=fig
+    )
 ])
 
 if __name__ == '__main__':
