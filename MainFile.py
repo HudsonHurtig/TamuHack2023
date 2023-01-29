9a28ccaf596d5c001849be8fdc8d7eebd4f2f129
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2023-01-28 19:36:09 -0600

    lazy commit message

--- MainFile.py
+++ MainFile.py
@@ -45,5 +45,13 @@ app.layout = html.Div(children=[
     html.Div(id='my-output')
 ])
 
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0',port=80)
