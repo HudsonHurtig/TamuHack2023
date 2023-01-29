96986ee4b224a1a3403518c112068b076d175105
Ahyee <113147823+Avi-Punjabi@users.noreply.github.com>
2023-01-29 01:05:17 -0600

    Add files via upload

--- yieldToMaturity.py
+++ yieldToMaturity.py
@@ -1,48 +1,46 @@
-from pprint import pprint
-import requests
-import os
-import urllib.parse
-import math as math
+import math
 
-appid = 'WUP7T2-WQKTTWWXU4'
+currPrice = float(input("Please input the current price: "))
+faceVal = float(input("Please input the face/par value: "))
+years = float(input("Please enter the years to maturity: "))
+coupRate = float(input("Please enter the annual coupon rate as a decimal (e.g. 5% = 0.05): "))
+freq = float(input("Please enter the frequency (per year) of coupon payment (e.g. semiannually is 2): "))
 
-while True:
-    try:
-        currPrice = input("Enter the current price: ")
-        faceVal = input("Enter the face/par value: ")
-        years = input("Enter the years to maturity: ")
-        coupRate = input("Enter the coupon rate as a decimal (e.g. 5% = 0.05): ")
-        freq = input("Enter the frequency (per year) of coupon payment (e.g. semianually is 2): ")
+def ytm(currPrice, faceVal, years, coupRate, freq, x):
+    c = currPrice
+    f = faceVal
+    n = years
+    r = coupRate
+    m = freq
+    
+    if r == 0:
+        return (f / c) ** (1 / n) - 1
+    else:
+        return c - (f / ((1 + x / m) ** (n * m)) + (f * r / m) / ((1 + x / m)) * (1 - (1 + x / m) ** (-n * m)) / (1 - (1 + x / m) ** (-1)))
 
-        equation = currPrice + "=" + faceVal + "/(1+x/" + freq + ")^(" + years + "*" + freq + ")+sum_(i=1)^(" + years + "*" + freq + ")(" + faceVal + "*" + coupRate + "/" + freq + ")/(1+x/" + freq + ")^i"
+def bisection(a, b, tol):
+    x = a
 
-        query = urllib.parse.quote_plus(f"{equation}")
-        query_url = f"http://api.wolframalpha.com/v2/query?" \
-                    f"appid={appid}" \
-                    f"&input={query}" \
-                    f"&includepodid=RealSolution" \
-                    f"&output=json"
+    while ((b - a) >= tol):
+        x = (a + b)/2
 
-        r = requests.get(query_url).json()
+        if ytm(currPrice, faceVal, years, coupRate, freq, x) == 0.0:
+            break
+        elif ytm(currPrice, faceVal, years, coupRate, freq, x) * ytm(currPrice, faceVal, years, coupRate, freq, a) < 0:
+            b = x
+        else:
+            a = x
 
-        data = r["queryresult"]["pods"][0]["subpods"][1]
+    return x
 
-        """
-        for key in r["queryresult"]:
-            if key == 'pods':
-                for key2 in r["queryresult"][key][0]:
-                    if key2 == 'subpods':
-                        dict = r["queryresult"][key][0][key2][1]
-                        for key3 in dict:
-                            if key3 == 'plaintext':
-                                print(dict[key3])
-        """
+if coupRate != 0:
+    a = 0.0000001
+    b = 1
+    tol = 0.0000001
+    root = bisection(a, b, tol)   
+else:
+    root = (faceVal / currPrice) ** (1 / years) - 1
 
-        result = str(float(data['plaintext'].split('≈')[1])*100) + '%'
+rootPercent = f'{root * 100:0.4f}%'
 
-        print("The yeild to maturity is " + result)
-
-        break
-
-    except:
-        print("Invalid Data, Try Again")
+print("Approximate solution is: " + rootPercent)
